3661d556a10c5681aeab9c2d53ebeb3dbf7fb962
Abhinav Sharma <abhinav@laso.ai>
2020-06-17 12:17:00 -0600

    history: get 1 week back

--- src/newtab.ts
+++ src/newtab.ts
@@ -10,10 +10,16 @@ interface HistoryItem {
   typedCount?: number
 }
 
+const days = 7;
+const microsecondsBack = 1000 * 60 * 60 * 24 * days;
+const startTime = (new Date).getTime() - microsecondsBack;
+
 window.addEventListener('DOMContentLoaded', (event) => {
-  chrome.history.search({'text': ''}, (historyItems: HistoryItem[]) => {
+  chrome.history.search({
+    'text': '', 
+    maxResults: 0, 
+    startTime: startTime}, (historyItems: HistoryItem[]) => {
     debugger;
   })
 });
 
-console.log(document.querySelector("body"))
